6a984076b1f27b9b0d2b4c00a07c3b63e88e3b4e
SanShet <sanshett@gmail.com>
2023-01-18 11:14:52 +0300

    rewiev fix

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,21 @@
 const overlayEl = document.querySelector(".popup");
-const openPopupButton = document.querySelector(".profile-info__edit-button");
+const editButton = document.querySelector(".profile__edit-button");
 const closePopupButton = overlayEl.querySelector(".popup__close-button");
 const saveButton = overlayEl.querySelector(".popup__save-button");
 const profileName = document.querySelector(".profile__title");
 const profileJob = document.querySelector(".profile__subtitle");
 
 const formElement = document.querySelector(".popup__container");
-const popupName = formElement.querySelector("#popup__name");
+const popupName = formElement.querySelector("#popup-name");
 const popupJob = formElement.querySelector("#popup-job");
 
-const toggleOverlay = () => {
-  overlayEl.classList.toggle("popup_opened");
-};
+function openPopup(popup) {
+  popup.classList.add("popup_opened");
+}
 
-openPopupButton.addEventListener("click", () => {
-  toggleOverlay();
-});
-
-closePopupButton.addEventListener("click", () => {
-  toggleOverlay();
-});
-
-saveButton.addEventListener("click", () => {
-  toggleOverlay();
-});
+function closePopup(popup) {
+  popup.classList.remove("popup_opened");
+}
 
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
@@ -31,7 +23,18 @@ function formSubmitHandler(evt) {
   evt.preventDefault();
   profileName.textContent = popupName.value;
   profileJob.textContent = popupJob.value;
+  closePopup(overlayEl);
 }
+
+editButton.addEventListener("click", () => {
+  openPopup(overlayEl);
+  popupName.value = profileName.textContent;
+  popupJob.value = profileJob.textContent;
+});
+
+closePopupButton.addEventListener("click", () => {
+  closePopup(overlayEl);
+});
 
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
